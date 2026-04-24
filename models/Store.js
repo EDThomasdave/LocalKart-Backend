726796617075561ae0e1a9ff06e34d909f9d3f6f
Chaitanya_1972 <chaitt724@gmail.com>
2026-04-25 03:37:19 +0530

    Update Store.js

--- models/Store.js
+++ models/Store.js
@@ -18,10 +18,19 @@ const storeSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+
+  // 🆕 ADD THIS
+  address: {
+    type: String,
+    required: true
+  },
+
+  // already correct
   location: {
     lat: Number,
     lng: Number
   }
+
 }, { timestamps: true });
 
 export default mongoose.model("Store", storeSchema);
