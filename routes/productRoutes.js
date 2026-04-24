10ea22a0370f5f12e0f2b2a371bdce5da0920cf7
Chaitanya_1972 <chaitt724@gmail.com>
2026-04-25 05:03:14 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   getProducts,
   addProduct,
-  addProductsBulk,
   deleteProduct
 } from "../controllers/productController.js";
 
@@ -10,16 +9,8 @@ import { upload } from "../middlewares/upload.js";
 
 const router = express.Router();
 
-// ✅ GET PRODUCTS
 router.get("/", getProducts);
-
-// ✅ ADD PRODUCT (WITH IMAGE UPLOAD)
 router.post("/", upload.single("image"), addProduct);
-
-// ✅ BULK INSERT
-router.post("/bulk", addProductsBulk);
-
-// ✅ DELETE
 router.delete("/:id", deleteProduct);
 
 export default router;
