b3f0e89d83ec9234bba33d352a8eef7f016a2716
Chaitanya_1972 <chaitt724@gmail.com>
2026-04-25 04:33:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ dotenv.config();
 
 const app = express();
 // 🔥 MUST BE FIRST MIDDLEWARE
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
+
 app.use(cors({
   origin: "*"
 }));
