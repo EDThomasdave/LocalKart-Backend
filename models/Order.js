d1572a31c710293b1280263cee5d3f8f15ce9b1f
Chaitanya_1972 <chaitt724@gmail.com>
2026-04-25 17:44:20 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,10 +1,29 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
+  userId: String,
+
   storeId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Store"
   },
+
+  items: [
+    {
+      name: String,
+      price: Number,
+      quantity: Number
+    }
+  ],
+
+  total: Number,
+
+  status: {
+    type: String,
+    enum: ["pending", "fulfilled", "completed"],
+    default: "pending"
+  },
+
   createdAt: {
     type: Date,
     default: Date.now
